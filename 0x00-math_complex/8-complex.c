de9fbef3b67fab29f92ebf6d6d31aa72f5916416
Diego <diegojeanluck@hotmail.com>
2022-03-18 11:11:24 -0500

    Feat advanced task #8 and works

--- 0x00-math_complex/8-complex.c
+++ 0x00-math_complex/8-complex.c
@@ -1,1 +1,18 @@
 #include "holberton.h"
+
+/**
+ * complex_from_mod_arg - Get the original complex number from
+ * it's modulus and argument.
+ *
+ * @m: Modulus of a complex number.
+ *
+ * @arg: Agtument of a complex number.
+ *
+ * Return: The modulus of a complex number.
+ */
+
+void complex_from_mod_arg(double m, double arg, complex *c3)
+{
+	c3->re = m * cos(arg); 	/*mod * cos(re/mod)*/
+	c3->im = m * sin(arg);	/*mod * sin(im/mod)*/
+}
